a81e9189d68b360ba1e94dee5cc6c9fdc4b94a63
Alexandr Chekalenko <whibleykevin@gmail.com>
2017-10-17 19:12:28 +0300

    ALL WORK

---
+++ src/main/java/com/test/bu/controller/ExchangeController.java
@@ -0,0 +1,18 @@
+package com.test.bu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/payments")
+public class ExchangeController {
+
+    @GetMapping("/exchange")
+    public String showExchangePage() {
+        return "exchangePage";
+    }
+
+
+}
+
---
+++ src/main/java/com/test/bu/controller/TransferController.java
@@ -0,0 +1,15 @@
+package com.test.bu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/payments")
+public class TransferController {
+
+    @GetMapping("transfer")
+    public String showTransferPage() {
+        return "transferPage";
+    }
+}
